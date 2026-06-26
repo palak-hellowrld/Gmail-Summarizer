9c79f31527793b5941d23414d59b28a9bdfbcbf1
Palak Mandloi <palakmandloi7@gmail.com>
2026-06-25 19:24:19 -0700

    Fake email data for UI testing

---
+++ app_UI.py
@@ -0,0 +1,7 @@
+myEmailList={"email1":{"sender": "mom@gmail.com", "subject": "This is Mom", "summary": "This is a summary of the email from Mom."},
+             "email2":{"sender": "dad@gmail.com", "subject": "This is Dad", "summary": "This is a summary of the email from Dad."},
+             "email3":{"sender": "brother@gmail.com", "subject": "This is Brother", "summary": "This is a summary of the email from Brother."},
+             "email4":{"sender": "sister@gmail.com", "subject": "This is Sister", "summary": "This is a summary of the email from Sister."}, 
+             "email5":{"sender": "myself@gmail.com", "subject": "This is Myself", "summary": "This is a summary of the email from Myself."}, 
+            } 
+print(myEmailList)
